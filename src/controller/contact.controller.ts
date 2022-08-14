366b42ea5c24b8267224543a5efae827e96656a4
Rafik Moreira <rafikmoreiradias@outlook.com>
2022-08-14 16:50:06 -0300

    add current user info

--- src/controller/contact.controller.ts
+++ src/controller/contact.controller.ts
@@ -1,31 +1,31 @@
-import { Contact } from '@prisma/client'
-import { Request, Response } from 'express'
+import { Contact } from '@prisma/client';
+import { Request, Response } from 'express';
 
-import { createContactService } from '../service/create-contact.service'
-import { Controller } from '../interfaces/controller'
+import { createContactService } from '../service/create-contact.service';
+import { Controller } from '../interfaces/controller';
 
 export class ContactController implements Controller {
   async list(req: Request, res: Response): Promise<Response> {
-    const contacts: Contact[] = []
-    return res.status(200).json(contacts)
+    const contacts: Contact[] = [];
+    return res.status(200).json(contacts);
   }
 
   async show(req: Request, res: Response): Promise<Response> {
-    return res.status(200).json()
+    return res.status(200).json();
   }
 
   async create(req: Request, res: Response): Promise<Response> {
-    const contact = req.body as Contact
-    const userId = 1
-    createContactService(contact, userId)
-    return res.status(200).json(contact)
+    const contact = req.body as Contact;
+    const userId = 1;
+    createContactService(contact, userId);
+    return res.status(200).json(contact);
   }
 
   async update(req: Request, res: Response): Promise<Response> {
-    return res.status(200).json()
+    return res.status(200).json();
   }
 
   async destroy(req: Request, res: Response): Promise<Response> {
-    return res.status(200).json()
+    return res.status(200).json();
   }
 }
